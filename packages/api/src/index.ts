dade98f5ef52efd30c1302f525e9122df9ab7462
sviryukov <sviryukov.k@phystech.edu>
2023-08-04 08:57:17 +0300

    fix(api): handle error in POST createAllContainersPDF method handler

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -17,20 +17,27 @@ const upload = multer();
 const railwayBillsPDFService = new RailwayBillsPDFService();
 
 app.post('/createAllContainersPDF', upload.any(), async (req, res) => {
-  if (!Array.isArray(req.files)) return res.status(400).send();
-  const xlsx = req.files.find((file) => file.fieldname === 'dataXlsx');
-  if (!xlsx) return res.status(400).send('No XLSX file');
-  const pdfs = req.files.filter((file) => /^pdfs\[\d*]$/.test(file.fieldname));
-  const transporters = Object.values(
-    unflatten(req.body) as { [prop: string]: unknown },
-  ) as TransporterData[];
-  const containers = await getContainersDataFromXLS(xlsx.buffer);
-  const resultPDFBuffer = await railwayBillsPDFService.createAllContainersPDF(
-    pdfs.map((file) => file.buffer),
-    containers,
-    transporters,
-  );
-  res.send(resultPDFBuffer);
+  try {
+    if (!Array.isArray(req.files)) return res.status(400).send();
+    const xlsx = req.files.find((file) => file.fieldname === 'dataXlsx');
+    if (!xlsx) return res.status(400).send('No XLSX file');
+    const pdfs = req.files.filter((file) =>
+      /^pdfs\[\d*]$/.test(file.fieldname),
+    );
+    const transporters = Object.values(
+      unflatten(req.body) as { [prop: string]: unknown },
+    ) as TransporterData[];
+    const containers = await getContainersDataFromXLS(xlsx.buffer);
+    const resultPDFBuffer = await railwayBillsPDFService.createAllContainersPDF(
+      pdfs.map((file) => file.buffer),
+      containers,
+      transporters,
+    );
+    res.send(resultPDFBuffer);
+  } catch (e) {
+    res.statusMessage = e?.message;
+    res.status(400).end();
+  }
 });
 
 const port = process.env.APP_PORT || 3001;
